8ac219dd24b8660c76512d99aa07dc6783e20870
sandy <27494131@qq.com>
2017-09-20 11:57:51 +0800

    local add test方法

--- src/main/java/com/sandy/demo/Demo.java
+++ src/main/java/com/sandy/demo/Demo.java
@@ -4,4 +4,9 @@ public class Demo {
     public static void main(String args[]){
         System.out.println("Hello World!!!");
     }
+
+    public static void test(){
+        int i=0;
+        System.out.println(i);
+    }
 }
